a1e055c20d2c56ce863a59ef4e610950aa87fbb7
Benson Gikonyo <muhiugikonyo@gmail.com>
2026-04-13 20:38:08 +0300

    initial commit

---
+++ blog/sitemaps.py
@@ -0,0 +1,26 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from .models import Post
+from taggit.models import Tag
+
+
+class PostSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.9
+
+    def items(self):
+        return Post.published.all()
+
+    def lastmod(self, obj):
+        return obj.updated
+
+
+class TagSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.6
+
+    def items(self):
+        return Tag.objects.all()
+
+    def location(self, obj):
+        return reverse("blog:post_list_by_tag", args=[obj.slug])
